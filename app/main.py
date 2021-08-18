214c00896297f36c06cac3da511fa943b07ede50
Akshatha Nayak <akshatha@ebi.ac.uk>
2021-08-18 21:22:16 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, Query
+from typing import Optional, List
+from elasticsearch import Elasticsearch
+from decouple import config
+from app.utils import generate_request_body, Index, \
+    parse_fields, serialize_record, generate_csv_file, DEFAULT_COLUMNS
+import json
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+NODE1 = config('NODE1')
+NODE2 = config('NODE2')
+es = Elasticsearch([NODE1, NODE2])
+
+@app.get("/search")
+def search_mulitple_indices(
+    indices: List[Index] = Query(None),
+    _source: Optional[str] = Query(None,
+        description='Provide comma-separated fields to fetch. \
+            For example, organism.text,sex.text'),
+    size: Optional[int] = 10, \
+    from_: Optional[int] = 0, \
+    sort: Optional[str] = Query(None,
+        description='Provide fields to sort by, in the format: \
+            field1:<asc|desc>,field2:<asc|desc>. For example, \
+                organism.text:asc,sex.text:desc'), \
+    filters: Optional[List[str]] = Query([], regex=".+=(.+,?)+",
+        description='Each filter condition should have the format: \
+            field_name=value1,value2. For example, \
+                organism.text=Sus scrofa,Gallus gallus'),
+    aggs: Optional[List[str]] = Query([], regex=".+=.+", 
+        description='Each aggregation item should have the format: \
+            agg_name=agg_field. For example, \
+                organisms=organism.text'),
+    q: Optional[str] = None):
+    data = es.search(index=indices, _source=_source,\
+        size=size, from_=from_, sort=sort, q=q,\
+        body=generate_request_body(filters, aggs))
+    return data
+
+@app.get("/columns")
+def get_columns_for_all_indices():
+    indices = DEFAULT_COLUMNS.keys()
+    res = {}
+    for index in indices:
+        mapping_file = "mapping/" + index + ".json"
+        f = open(mapping_file)
+        data = json.load(f)
+        f.close()
+        res[index] = {
+            'columns': parse_fields(data, [], []),
+            'defaults': DEFAULT_COLUMNS[index]
+        }
+    return res
+
+@app.get("/download")
+def download_as_CSV(
+    indices: List[Index] = Query(None),
+    _source: Optional[str] = Query(None,
+        description='Provide comma-separated fields to fetch. \
+            For example, organism.text,sex.text'),
+    sort: Optional[str] = Query(None,
+        description='Provide fields to sort by, in the format: \
+            field1:<asc|desc>,field2:<asc|desc>. For example, \
+                organism.text:asc,sex.text:desc'), \
+    filters: Optional[List[str]] = Query([], regex=".+=(.+,?)+",
+        description='Each filter condition should have the format: \
+            field_name=value1,value2. For example, \
+                organism.text=Sus scrofa,Gallus gallus'),
+    q: Optional[str] = None):
+    SIZE = 1000000
+    data = es.search(index=indices, _source=_source, size=SIZE, sort=sort, \
+        q=q, body=generate_request_body(filters, []))
+    records = list(map(lambda rec: rec['_source'], data['hits']['hits']))
+    records = list(map(lambda rec: serialize_record(rec, rec, []), records))
+    generate_csv_file(records, _source.split(','))
+    return FileResponse('data.csv', media_type='text/csv',filename='data.csv')
---
+++ app/utils.py
@@ -0,0 +1,184 @@
+from enum import Enum
+import csv
+
+class Index(str, Enum):
+    file = "file"
+    organism = "organism"
+    specimen = "specimen"
+    dataset = "dataset"
+    article = "article"
+    analysis = "analysis"
+    experiment = "experiment"
+    protocol_files = "protocol_files"
+    protocol_samples = "protocol_samples"
+    protocol_analysis = "protocol_analysis"
+
+DEFAULT_COLUMNS = {
+        'file': [
+            'study.accession',
+            'experiment.accession',
+            'species.text',
+            'experiment.assayType',
+            'experiment.target',
+            'specimen',
+            'run.instrument',
+            'experiment.standardMet',
+            'paperPublished'
+        ],
+        'organism': [
+            'biosampleId',
+            'sex.text', 
+            'organism.text',
+            'breed.text', 
+            'standardMet',
+            'paperPublished'
+        ],
+        'specimen': [
+            'biosampleId', 
+            'material.text', 
+            'cellType.text',
+            'organism.sex.text',  
+            'organism.organism.text',
+            'organism.breed.text',
+            'standardMet',
+            'paperPublished', 
+        ],
+        'dataset': [
+            'accession',
+            'title',
+            'species.text',
+            'archive',
+            'assayType',
+            'standardMet',
+            'paperPublished'
+        ],
+        'article': [
+            'title',
+            'year',
+            'datasetSource',
+            'journal'
+        ],
+        'analysis': [
+            'accession',
+            'datasetAccession',
+            'title',
+            'organism.text',
+            'assayType',
+            'analysisType',
+            'standardMet'
+        ],
+        'experiment': [
+            'accession',
+            'assayType',
+            'experimentTarget',
+            'standardMet'
+        ],
+        'protocol_files': [
+            'name',
+            'experimentTarget',
+            'assayType',
+        ],
+        'protocol_samples': [
+            'protocolName',
+            'key',
+            'universityName',
+            'protocolDate'
+        ],
+        'protocol_analysis': [
+            'analysisType',
+            'protocolName',
+            'key',
+            'universityName',
+            'protocolDate'
+        ]
+    }
+
+def generate_request_body(filters, aggs):
+    body = {}
+    # generate query for filtering
+    filter_values = []
+    not_filter_values = []
+    for filter in filters:
+        # each filter is of the following format- 
+        # organism.text=Sus scrofa,Gallus gallus, ...
+        filter = filter.split('=')
+        key = filter[0]
+        val = filter[1].split(',')
+        if val[0] != 'false':
+            filter_values.append({"terms": {key: val}})
+        else:
+            not_filter_values.append({"terms": {key: ["true"]}})
+    filter_val = {}
+    if filter_values:
+        filter_val['must'] = filter_values
+    if not_filter_values:
+        filter_val['must_not'] = not_filter_values
+    if filter_val:
+        body['query'] = {"bool": filter_val}
+
+    # generate query for aggregations
+    agg_values = {}
+    for agg in aggs:
+        # each agg is of the following format- 
+        # agg_name=field to aggregate on
+        key, val = agg.split('=')
+        # size determines number of aggregation buckets returned
+        agg_values[key] = {"terms": {"field": val, "size": 25}} 
+        if key == 'paper_published':
+            # aggregations for missing paperPublished field
+            agg_values["paper_published_missing"] = {
+                "missing": {"field": "paperPublished"}}
+
+    body['aggs'] = agg_values
+    return body
+
+def parse_fields(data, mapping, properties):
+    data = data['properties']
+    for prop in data:
+        properties.append(prop)
+        if 'properties' in data[prop]:
+            mapping = parse_fields(data[prop], mapping, properties)
+        else:
+            mapping.append('.'.join(properties))
+        properties.pop()
+    return mapping
+
+def serialize_record(parentRecord, currentRecord, parentProps):
+    for prop in currentRecord.copy():
+        parentProps.append(prop)
+        if type(currentRecord[prop]) is list and len(currentRecord[prop]):
+            currentRecord[prop] = flatten_list(currentRecord[prop])
+        if type(currentRecord[prop]) is dict:
+            serialize_record(parentRecord, currentRecord[prop], parentProps)
+        else:
+            serailisedProp = '.'.join(parentProps)
+            parentRecord[serailisedProp] = currentRecord[prop]
+        parentProps.pop()
+    return parentRecord
+
+def flatten_list(items):
+    if type(items[0]) is dict:
+        result = {}
+        for i in range(len(items)):
+            for prop in items[i]:
+                if prop in result:
+                    result[prop] = result[prop] + ',\n' + items[i][prop]
+                else:
+                    result[prop] = items[i][prop]
+        return result
+    else:
+        return ',\n'.join(items)
+
+def delete_extra_fields(record, fields):
+    updated_record = {}
+    for prop in record.keys():
+        if prop in fields:
+            updated_record[prop] = record[prop]
+    return updated_record
+
+def generate_csv_file(records, columns):
+    records = list(map(lambda rec: delete_extra_fields(rec, columns), records))
+    with open('data.csv', 'w') as csvfile: 
+        dict_writer = csv.DictWriter(csvfile, columns)
+        dict_writer.writeheader()
+        dict_writer.writerows(records)
